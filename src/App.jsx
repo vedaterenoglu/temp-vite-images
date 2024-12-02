bd50c52f0f984142016e2a54c62a738f4810a02d
gtbs-coding <gtbs.se@gmail.com>
2024-12-02 14:38:16 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/** @format */
+
+import SearchForm from './SearchForm'
+import Gallery from './Gallery'
+import ThemeToggle from './ThemeToggle'
+
+const App = () => {
+	return (
+		<main>
+			<ThemeToggle />
+			<SearchForm />
+			<Gallery />
+		</main>
+	)
+}
+export default App
